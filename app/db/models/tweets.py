e756664422f4b702056f0b6904d66109b7fec0fa
Fabio Pardo <fpardo1023@gmail.com>
2024-05-06 11:46:18 -0400

    alembic migrations to handle large integer unique id, let's make it string. Also hateful isn't boolean, it's a string.

--- app/db/models/tweets.py
+++ app/db/models/tweets.py
@@ -5,7 +5,7 @@ from app.db.database import Base
 class Tweet(Base):
     __tablename__ = "tweets"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(String, primary_key=True)
 
     author = Column(String, index=True)
     author_created_utc = Column(DateTime)
@@ -15,7 +15,7 @@ class Tweet(Base):
     day = Column(Integer, index=True)
     follower_count = Column(Integer)
     full_text = Column(String)
-    hateful = Column(Boolean, default=False, index=True)
+    hateful = Column(String, default=False, index=True)
     lang = Column(String, index=True)
     len_filter = Column(Boolean, default=False, index=True)
     minute = Column(Integer, index=True)
