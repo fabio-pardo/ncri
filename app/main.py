b11d12cde199cf6df39ea6632e3cb0c2823c84ec
Fabio Pardo <fpardo1023@gmail.com>
2024-05-06 16:23:31 -0400

    add visualization_data to routers

--- app/main.py
+++ app/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 
 from app.api.data_filtering import data_filtering
 from app.api.analytics import analytics
+from app.api.visualization_data import visualization_data
 
 app = FastAPI()
 app.include_router(data_filtering)
 app.include_router(analytics)
+app.include_router(visualization_data)
 
 
 @app.get("/")
